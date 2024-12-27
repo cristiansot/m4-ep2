0902d9ce85bddcf2a1f7c667eb2ea91427890309
cristiansot <soluciongrafix@gmail.com>
2024-12-27 14:38:07 -0300

    HOC listo

--- src/components/DoctorCard.jsx
+++ src/components/DoctorCard.jsx
@@ -1,48 +1,43 @@
-import React, { useEffect, useState } from "react";
-import PropTypes from "prop-types";
+import React from 'react';
+import PropTypes from 'prop-types';
+import withModal from '../hoc/withModal';
+import Modal from './Modal';
 
-const DoctorCard = ({ doctor }) => {
-  const [imageUrl, setImageUrl] = useState("");
-
-  useEffect(() => {
-    const fetchImage = () => {
-      if (doctor.imagen) {
-        setImageUrl(doctor.imagen); 
-      } else {
-        console.warn(`No se encontró imagen para el doctor: ${doctor.nombre}`);
-        setImageUrl("/default-image.jpg"); 
-      }
-    };
-
-    fetchImage();
-  }, [doctor.imagen]);
+const DoctorCard = ({ doctor, isModalOpen, openModal, closeModal }) => {
+  const modalContent = {
+    title: `${doctor.nombre}`,
+    especialidad: `Especialidad: ${doctor.especialidad}`,
+    resumen: `${doctor.resumen}`,
+  };
 
   return (
     <div className="doctor-card">
       <img
-        src={imageUrl}
+        src={doctor.imagen}
         alt={`Foto de ${doctor.nombre}`}
         className="doctor-image"
         style={{ height: 400, borderRadius: 30 }}
       />
       <div className="doctor-info">
-        <h2 className="doctor-name">{doctor.nombre}</h2>
-        <p className="doctor-specialty">Especialidad: {doctor.especialidad}</p>
-        <p className="doctor-experience">
-          Años de experiencia: {doctor.años_experiencia}
-        </p>
+        <h2>{doctor.nombre}</h2>
+        <button onClick={openModal} style={{ marginTop: -10 }}>Ver detalles</button>
       </div>
+
+      <Modal isOpen={isModalOpen} content={modalContent} onClose={closeModal} />
     </div>
   );
 };
 
 DoctorCard.propTypes = {
   doctor: PropTypes.shape({
-    nombre: PropTypes.string.isRequired, 
-    imagen: PropTypes.string.isRequired, 
-    especialidad: PropTypes.string.isRequired, 
-    años_experiencia: PropTypes.number.isRequired, 
+    nombre: PropTypes.string.isRequired,
+    imagen: PropTypes.string.isRequired,
+    especialidad: PropTypes.string.isRequired,
+    resumen: PropTypes.string.isRequired,
   }).isRequired,
+  isModalOpen: PropTypes.bool.isRequired,
+  openModal: PropTypes.func.isRequired,
+  closeModal: PropTypes.func.isRequired,
 };
 
-export default DoctorCard;
+export default withModal(DoctorCard);
